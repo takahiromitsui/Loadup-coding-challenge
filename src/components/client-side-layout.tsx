1186526147565103cbd1373e718529a76805fdde
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2025-01-28 15:55:56 +0100

    feat: create layout for sidebar and navbar

---
+++ src/components/client-side-layout.tsx
@@ -0,0 +1,47 @@
+'use client';
+import { Grid, GridItem, useBreakpointValue } from '@chakra-ui/react';
+
+export default function ClientSideLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	const showSidebar = useBreakpointValue({ base: false, lg: true });
+	return (
+		<Grid templateColumns='repeat(6, 1fr)' bg='gray.50'>
+			{showSidebar ? (
+				<GridItem
+					as='aside'
+					colSpan={{
+						base: 6,
+						lg: 2,
+						xl: 1,
+					}}
+					bg='gray.100'
+					minHeight={{
+						lg: '100vh',
+					}}
+					p={{
+						base: '20px',
+						lg: '40px',
+					}}
+				>
+					<>Side bar</>
+				</GridItem>
+			) : null}
+
+			<GridItem
+				as='main'
+				colSpan={{
+					base: 6,
+					lg: 4,
+					xl: 5,
+				}}
+				p='40px'
+			>
+				<>Navbar</>
+				{children}
+			</GridItem>
+		</Grid>
+	);
+}
